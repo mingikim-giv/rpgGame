6313dbdd4a4e5b14f49ed017813c6ca634a07c1d
mingikim-giv <mingikim.giv@gmail.com>
2024-04-04 15:04:19 +0900

    Add: Monster Class(getter & setter)

--- rpgGame/src/rpgGame/Monster.java
+++ rpgGame/src/rpgGame/Monster.java
@@ -1,5 +1,34 @@
 package rpgGame;
 
 public class Monster {
-
+	private int hp;
+	private int maxHp;
+	private int power;
+	private String name;
+	
+	public Monster() {}
+	
+	public Monster(String name, int maxHp, int pw) {
+	    this.name = name;
+	    this.hp = maxHp;
+	    this.maxHp = maxHp;
+	    this.power = pw;
+	}
+	
+	public int getHp() {
+		return this.hp;
+	}
+	public int getMaxHp() {
+		return this.maxHp;
+	}
+	public int getPower() {
+		return this.power;
+	}
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setHp(int hp) { 
+		this.hp = hp;
+	}
 }
