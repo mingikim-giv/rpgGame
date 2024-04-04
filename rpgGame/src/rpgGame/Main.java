8cadab82aee603adeb2f6a7dd7e6aa0f75ce56d0
mingikim-giv <mingikim.giv@gmail.com>
2024-04-04 10:07:18 +0900

    Add: Main Class(GameManager new)

--- rpgGame/src/rpgGame/Main.java
+++ rpgGame/src/rpgGame/Main.java
@@ -3,8 +3,8 @@ package rpgGame;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameManager system = new GameManager();
+		system.run();
 	}
 
 }
