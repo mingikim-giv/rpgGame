01b896c0162158198a2b2db2d60f9c30ac2d6c0e
mingikim-giv <mingikim.giv@gmail.com>
2024-04-04 10:56:43 +0900

    Add: StageTitle Class

--- rpgGame/src/rpgGame/StageTitle.java
+++ rpgGame/src/rpgGame/StageTitle.java
@@ -1,5 +1,25 @@
 package rpgGame;
 
-public class StageTitle {
+public class StageTitle extends Stage {
 
+	@Override
+	public boolean update() {
+		System.out.println("🏰스톰빌 성🏰");
+		System.out.println("[시작]을 입력하세요.");
+		String start = GameManager.scan.next();
+		
+		while(!start.equals("시작")) {
+			System.out.println("[시작]을 입력하세요.");
+			start = GameManager.scan.next();
+		}
+		GameManager.nextStage = "로비";
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
