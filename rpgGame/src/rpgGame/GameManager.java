9eb53bee4c14067e014695b30df9f2042330a6f3
mingikim-giv <mingikim.giv@gmail.com>
2024-04-04 10:28:25 +0900

    Add: GameManager Class(changeStage, run)

--- rpgGame/src/rpgGame/GameManager.java
+++ rpgGame/src/rpgGame/GameManager.java
@@ -1,5 +1,51 @@
 package rpgGame;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameManager {
+	private Map<String, Stage> stageList = new HashMap<String, Stage>();
+	private boolean isRun = true;
 	
+	private static String nextStage = "";
+	private String curStage = "";
+	
+	// changeStage
+	private boolean changeStage() {
+		System.out.println("현재 스테이지:" + curStage);
+		System.out.println("다음 스테이지:" + nextStage);
+		
+		if(curStage.equals(nextStage)) {
+			return true;
+		}
+		
+		curStage = nextStage;
+		Stage stage = stageList.get(curStage);
+		stage.init();	// 스테이지 초기화
+		
+		while(true) {
+			isRun = stage.update();
+			if(isRun == false) {
+				break;
+			}
+		}
+		
+		if(nextStage.equals("")) {
+			return false;
+		}
+		else {
+			return true;
+		}
+		
+	}
+	
+	public void run() {
+		while(isRun) {
+			isRun = changeStage();
+			if(isRun == false) {
+				break;
+			}
+		}
+		System.out.println("GAME OVER");
+	}
 }
