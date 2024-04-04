1c17f220f86aa2c2a7158e2bdf7e89c4eda4d8e6
mingikim-giv <mingikim.giv@gmail.com>
2024-04-04 12:23:38 +0900

    Add: Unit Class(setItem, printStatus)

--- rpgGame/src/rpgGame/Unit.java
+++ rpgGame/src/rpgGame/Unit.java
@@ -29,6 +29,7 @@ abstract public class Unit {
 		ring = null;
 	}
 	
+	// overloading
 	public Unit(String name, int lv, int hp, int pw, int df, int exp, boolean party) {
 		super();
 		this.name = name;
@@ -44,4 +45,39 @@ abstract public class Unit {
 		ring = null;
 	}
 	
+	// setItem
+	public void setItem(Item weapon, Item armor, Item ring) {
+		this.weapon = weapon;
+		this.armor = armor;
+		this.ring = ring;
+	}
+	
+	// status
+	public void printStatus() {
+		System.out.printf("[이름:%s]", name);
+		System.out.printf("[레벨:%d]", level);
+		
+		// HP: hp + ring
+		if(ring != null) {
+			System.out.printf("[HP:%d+%d/%d+%d]", hp, ring, maxHp, ring);
+		}
+		else {
+			System.out.printf("[HP:%d/%d]", hp, maxHp);
+		}
+		// power: power + weapon
+		if(weapon != null) {
+			System.out.printf("[공격력:%d+%d]", power, weapon);
+		}
+		else {
+			System.out.printf("[공격력:%d]", power);
+		}
+		// defense: defense + armor
+		if(armor != null) {
+			System.out.printf("[방어력:%d+%d]", defense, armor);
+		}
+		else {
+			System.out.printf("[방어력:%d]", defense);
+		}
+		System.out.printf("[파티중:%s]\n", party);
+	}
 }
