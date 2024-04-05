2be90a713425a6340ee5604aeb8b9e4b5976adf0
mingikim-giv <mingikim.giv@gmail.com>
2024-04-05 11:26:46 +0900

    Add: StageBattle Class(printUnit Method)

--- rpgGame/src/rpgGame/StageBattle.java
+++ rpgGame/src/rpgGame/StageBattle.java
@@ -1,10 +1,33 @@
 package rpgGame;
 
-public class StageBattle extends Stage {
+import java.util.ArrayList;
 
+public class StageBattle extends Stage {
+	private UnitManager unitManager;
+	private ArrayList<Monster> monList = null;
+	
+	private int monDead;
+	private int playerDead;
+	
+	private boolean isRun = true;
+	private boolean isTurn = true;
+	
+	public StageBattle() {
+		unitManager = UnitManager.getInstance();
+		monDead = 0;
+		playerDead = 0;
+	}
+	
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		int playerIdx = 0;
+		int monIdx = 0;
+		
+		while(isRun) {
+			if(isTurn) {
+				
+			}
+		}
 		return false;
 	}
 
@@ -13,5 +36,18 @@ public class StageBattle extends Stage {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	// printUnit
+	private void printUnit() {
+		System.out.println("────────[⚔️전투⚔️]────────");
+		System.out.printf("%s : %s\n", playerDead, monDead);
+		System.out.println("───────[🕹️플레이어🕹️]───────");
+		for(int i = 0; i < Player.getGuildSize(); i ++) {
+			Player.getGuildUnit(i).printData();
+		}
+		System.out.println("───────[👾몬스터👾]───────");
+		for(int i = 0; i < monList.size(); i ++) {
+			monList.get(i).printData();
+		}
+	}
 }
