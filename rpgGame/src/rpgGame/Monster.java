f1741518fdb679a614733af6dfed5a103afacc09
mingikim-giv <mingikim.giv@gmail.com>
2024-04-05 16:16:00 +0900

    Modify: Monster Class

--- rpgGame/src/rpgGame/Monster.java
+++ rpgGame/src/rpgGame/Monster.java
@@ -57,8 +57,8 @@ abstract public class Monster {
 		System.out.printf("[%s](이)가 [%s]에게 %d의 데미지를 입힌다.\n", name, target.getName(), (this.power - target.getDefense()));
 		
 		if(target.getHp() <= 0) {
-			System.out.printf("[%s](를)을 처치했습니다.\n", target.getName());
 			target.setHp(0);
+			System.out.printf("[%s](를)을 처치했습니다.\n", target.getName());
 		}
 	}
 	
