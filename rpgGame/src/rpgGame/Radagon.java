1d07e329fc0cb1d6bc242103db82e291bc5e1a5b
mingikim-giv <mingikim.giv@gmail.com>
2024-04-08 11:42:02 +0900

    Add: Radagon Class(skill)

--- rpgGame/src/rpgGame/Radagon.java
+++ rpgGame/src/rpgGame/Radagon.java
@@ -5,7 +5,14 @@ public class Radagon extends Monster {
 		setName("라다곤");
 	}
 	
-	public void skill() {
+	public void skill(Player target) {
+		System.out.println("황금률 망치🔨");
+		target.setHp(target.getHp() - this.getPower()*3);
+		System.out.printf("[%s](이)가 플레이어에게 %d의 데미지를 입힌다.\n", this.getName(), (this.getPower()*3));
 		
+		if(target.getHp() <= 0) {
+			target.setHp(0);
+			System.out.printf("[%s](을)를 처치했습니다.\n", target.getName());
+		}
 	}
 }
