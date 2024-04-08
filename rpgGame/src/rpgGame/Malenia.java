0d8acf6ec79b34a7235335f0d695bd8324377de1
mingikim-giv <mingikim.giv@gmail.com>
2024-04-08 11:19:15 +0900

    Add: Malenia Class(skill)

--- rpgGame/src/rpgGame/Malenia.java
+++ rpgGame/src/rpgGame/Malenia.java
@@ -5,7 +5,14 @@ public class Malenia extends Monster {
 		setName("말레니아");
 	}
 	
-	public void skill() {
+	public void skill(Player target) {
+		System.out.println("물새 난격🗡️");
+		target.setHp(target.getHp() - this.getPower()*2);
+		System.out.printf("[%s](이)가 플레이어에게 %d의 데미지를 입힌다.\n", this.getName(), (this.getPower()*2));
 		
+		if(target.getHp() <= 0) {
+			target.setHp(0);
+			System.out.printf("[%s](을)를 처치했습니다.\n", target.getName());
+		}
 	}
 }
