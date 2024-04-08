13c8132ffa659a4131680351dc0de9cc0ccd2b8f
mingikim-giv <mingikim.giv@gmail.com>
2024-04-08 10:22:31 +0900

    Add: FileData Class(save)

--- rpgGame/src/rpgGame/FileData.java
+++ rpgGame/src/rpgGame/FileData.java
@@ -1,5 +1,19 @@
 package rpgGame;
 
-public class FileData {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class FileData {
+	private UnitManager unitManager;
+	private FileWriter fw = null;
+	private ArrayList<Player> temp;
+	
+	public void save() throws IOException {
+		unitManager = UnitManager.getInstance();
+		String data = "ELDEN.txt";
+		fw = new FileWriter(data);
+		temp = unitManager.player.getGuildList();
+		String gameData = "";
+	}
 }
